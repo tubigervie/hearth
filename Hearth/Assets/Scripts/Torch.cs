21f02e16d04a6627786d82558e5fbfe46f4da123
Evan Cheng <dr.frolicks@gmail.com>
2019-01-26 12:50:39 -0800

    Torch Duration and 'cheat code' toggle for testing

---
+++ Hearth/Assets/Scripts/Torch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events; 
+
+public class Torch : MonoBehaviour
+{
+    public float maxDuration; 
+    public float litTimeRemaining;
+    public bool lit; 
+
+    public UnityEvent onLit; 
+
+
+    [SerializeField]
+    ParticleSystem fireParticleSystem;
+    [SerializeField]
+    Light torchLight;
+
+    public void setLit(bool _lit = true)
+    {
+        if(_lit)
+        {
+            litTimeRemaining = maxDuration;
+            torchLight.enabled = true;
+            fireParticleSystem.Play();
+        }
+        else
+        {
+            litTimeRemaining = 0;
+            torchLight.enabled = false;
+            fireParticleSystem.Stop();
+        }
+        lit = _lit;
+        onLit.Invoke();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(lit)
+        {
+            if (litTimeRemaining > 0) litTimeRemaining -= Time.deltaTime; 
+            else setLit(false);  
+        }
+    }
+}
