9ce6d9c1c63ee7511f126238a88068386158cad3
Calvin Phung <RynsArgent@hotmail.com>
2019-01-26 20:36:53 -0800

    Add danger sound and improve gem sound

---
+++ Hearth/Assets/Scripts/Audio/DangerParameterExample.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FMODUnity;
+
+/// <summary>
+/// Expected to be attached on the game object with the event emitter which has
+/// the "threat" parameter.
+/// </summary>
+public class DangerParameterExample : MonoBehaviour
+{
+    private StudioEventEmitter emitter;
+
+    public float threat;
+
+    public void OnEnable()
+    {
+        emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+    }
+
+    public void Update()
+    {
+        emitter.SetParameter("threat", threat);
+    }
+}
