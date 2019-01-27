efb49b9563a3562dcc640b932e5732b19823ff36
tubigervie <etubig@uci.edu>
2019-01-27 10:22:06 -0800

    Hearth Map fixes

--- Hearth/Assets/Scripts/Inventory/InteractableItem.cs
+++ Hearth/Assets/Scripts/Inventory/InteractableItem.cs
@@ -14,7 +14,6 @@ public class InteractableItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
