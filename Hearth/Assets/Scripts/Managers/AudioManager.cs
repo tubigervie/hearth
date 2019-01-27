5cb445fce939e3b2fe723821ad1e10bd235256bb
Calvin Phung <RynsArgent@hotmail.com>
2019-01-27 11:49:29 -0800

    CP Add terrible weather but do not hook up

--- Hearth/Assets/Scripts/Managers/AudioManager.cs
+++ Hearth/Assets/Scripts/Managers/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class AudioManager : MonoBehaviour
 {
@@ -18,12 +19,51 @@ public class AudioManager : MonoBehaviour
     public GameObject gemCPrefab;
     public GameObject gemDPrefab;
     public GameObject gemSequencePrefab;
+    public GameObject terribleWeatherPrefab;
+
+    private GameObject terribleWeatherInstance;
+
+    private float currentThreatPct = 0.0f;
 
     public void Start()
     {
         if (windAmbiencePrefab != null)
         {
             GameObject.Instantiate(windAmbiencePrefab, this.transform);
+        }
+
+        if (terribleWeatherPrefab != null)
+        {
+            terribleWeatherInstance =
+                GameObject.Instantiate(terribleWeatherPrefab, this.transform);
+        }
+    }
+
+    public void Update()
+    {
+        float targetThreatPct = SessionManager.singleton.darknessTimer < 4.9f
+            ? 1.0f
+            : 0.0f;
+        if (currentThreatPct < targetThreatPct)
+        {
+            currentThreatPct = 
+                Mathf.MoveTowards(currentThreatPct, targetThreatPct, Time.deltaTime);
+        }
+        else if (currentThreatPct > targetThreatPct)
+        {
+            // Decay faster.
+            currentThreatPct =
+                Mathf.MoveTowards(currentThreatPct, targetThreatPct, 0.5f * Time.deltaTime);
+        }
+        SetTerribleWeatherIntensity(currentThreatPct);
+    }
+
+    private void SetTerribleWeatherIntensity(float value)
+    {
+        if (terribleWeatherInstance != null)
+        {
+            StudioEventEmitter emitter = terribleWeatherInstance.GetComponent<StudioEventEmitter>();
+            emitter.SetParameter("threat", value);
         }
     }
 }
