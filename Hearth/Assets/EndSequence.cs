af202b763b0b86e78c34a2c2d38a0a8ac3251c5a
tubigervie <etubig@uci.edu>
2019-01-26 23:41:36 -0800

    End SEquence

---
+++ Hearth/Assets/EndSequence.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndSequence : MonoBehaviour
+{
+    public static EndSequence singleton;
+    public float fadeOutTime;
+    Transform currentCameraTransform;
+    public Image whiteScreen;
+    public ObeliskDuder od;
+    Vector3 cameraVel;
+    public bool startEndCameraPan;
+    public float smoothDampTime = 1;
+
+    public void EndGame()
+    {
+        Camera.main.GetComponentInParent<CameraManager>().followPlayer = false;
+        startEndCameraPan = true;
+        currentCameraTransform = Camera.main.transform;
+        //titleBanner.CrossFadeAlpha(0, fadeOutTime, false);
+    }
+
+    private void Awake()
+    {
+        singleton = this;
+    }
+
+    private void Start()
+    {
+        Debug.Log("WTFFFF");
+        whiteScreen.canvasRenderer.SetAlpha(0);
+    }
+
+    private void Update()
+    {
+        if(startEndCameraPan)
+        {
+            Vector3 newPos = od.transform.position;
+            newPos.y = 15;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, newPos, Time.deltaTime * 1f);
+            if(Vector3.Distance(Camera.main.transform.position, newPos) <= 1f)
+            {
+                Debug.Log("Start fading out!");
+                whiteScreen.CrossFadeAlpha(1, fadeOutTime, false);
+                startEndCameraPan = false;
+            }
+        }
+    }
+}
